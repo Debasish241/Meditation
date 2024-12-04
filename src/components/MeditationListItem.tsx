f9655ed6b334a2f3e40d526a19486a27ad69cadd
DebasishDebnath <debasish.debnath@iemlabs.com>
2024-12-04 18:31:49 +0530

    update

--- src/components/MeditationListItem.tsx
+++ src/components/MeditationListItem.tsx
@@ -1,14 +1,14 @@
 import { Text, View } from "react-native";
 import { Meditation } from "../types";
-
-export function MeditationListItem({
-  meditation,
-}: {
-  meditation: Meditation;
-}) {
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+export function MeditationListItem({ meditation }: { meditation: Meditation }) {
   return (
     <View className="p-5 border-2 border-gray-300 rounded-2xl">
-      <Text className="font-semibold text-xl ">{meditation.title}</Text>
+      <Text className="font-semibold text-2xl ">{meditation.title}</Text>
+      <View className="flex-row items-center gap-1">
+        <FontAwesome6 name="clock" size={24} color="black" />
+        <Text className="text-gray-600">{meditation.duration} min</Text>
+      </View>
     </View>
   );
 }
